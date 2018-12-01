160a1f13099e3f10fc65983a735c0d6012af2626
Vishal Chandra <vishalchandra724@gmail.com>
2018-12-01 17:35:35 -0500

    fix KeyError caused by .read_csv()

--- plotter.py
+++ plotter.py
@@ -8,22 +8,24 @@ grades = {}
 first_iteration = True
 for csv in os.scandir('CSVs'):
 
-    data = pd.read_csv(csv.path)
-    #date = csv.path.split('/')[1].split('.')[0].split(' ')[0]
-    date = csv.path.split('.')[0]
+    data = pd.DataFrame.from_csv(csv.path)
+    date = csv.path.split('/')[1].split('.')[0].split(' ')[0]
     dates.append(date)
 
     if first_iteration:
         for index, row in data.iterrows():
             # create a new key value pair in grades with key as course name and value as a list of ints
             # splitting at space and taking first value to disregard letter 
-            grades[row['Description']] = [row['Term Performance'].split(' ')[0]]
+            #grades[row['Description']] = [row['Term Performance'].split(' ')[0]]
+            grades[row['Description']] = [row['Term Performance']] # test data has no letters
         first_iteration = False
     
     else:
         for index, row in data.iterrows():
             # add grade to corresponding list
-            grades[row['Description']].append(row['Term Performance'].split(' ')[0])
+            # splitting at space and taking first value to disregard letter 
+            # grades[row['Description']].append(row['Term Performance'].split(' ')[0])
+            grades[row['Description']].append(row['Term Performance']) # test data has no letters
 
 legend = []
 for key in grades:
