b45217de14969382edf9aad812b1c08a12b0f845
Vishal Chandra <vishalchandra724@gmail.com>
2018-12-31 17:16:20 -0500

    condense scraping code

--- Python/scraper.py
+++ Python/scraper.py
@@ -11,30 +11,25 @@ def scrape() -> pd.DataFrame:
     Logs into Follett Aspen and scrapes HTML table containing grades and course information
     """
     
-    #opening chrome in headless mode and going to aspen: https://duo.com/decipher/driving-headless-chrome-with-python
+    #opening chrome in headless mode (no GUI) and going to aspen: https://duo.com/decipher/driving-headless-chrome-with-python
     chrome_options = Options()  
     chrome_options.add_argument("--headless") 
     browser = webdriver.Chrome(options=chrome_options)
     browser.get('https://ma-concord.myfollett.com/aspen/logon.do')
 
-    #username
-    username_field = browser.find_element_by_name('username')
-    #set username as bash environment variable for security on github - can be replaced with string
-    username_field.send_keys(os.getenv('ASPEN_USERNAME'))
+    #username & password - values stored as bash environment variables (can be replaced with string)
+    username_field = browser.find_element_by_name('username').send_keys(os.getenv('ASPEN_USERNAME'))
+    password_field = browser.find_element_by_name('password').send_keys(os.getenv('ASPEN_PASSWORD'))
 
-    #password
-    password_field = browser.find_element_by_name('password')
-    #set password as bash environment variable for security on github- can be replaced with string
-    password_field.send_keys(os.getenv('ASPEN_PASSWORD'))
-
-    #click on "Log On ->"
-    login_button = browser.find_element_by_id('logonButton')
-    login_button.click()
+    #          ___________
+    #click on | Log On -> |
+    #          ‾‾‾‾‾‾‾‾‾‾‾         
+    browser.find_element_by_id('logonButton').click()
 
     #go to academics tab
     browser.get('https://ma-concord.myfollett.com/aspen/portalClassList.do?navkey=academics.classes.list')
 
-    #save all tables from the html, then pick out 25th table (contains the grades)
+    #read all tables from the html and save 25th table (contains the grades)
     grades = pd.read_html(browser.page_source)[24]
 
     browser.quit()
